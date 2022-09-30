4a18d220596ff83811a1f63f101a09fb1f7a5e0f
Alexander <pogorelov-alserg@yandex.ru>
2022-09-30 12:35:09 +0300

    add ability to create card number with spaces (#9)
    * remove spaces from input card number
    
    todo add tests
    
    #2
    
    * add tests for CardNumber.create
    
    - add tests project using FsCheck
    - add property "CardNumber.create is equals for 16 digits string and same string with spaces every 4 digits"
    
    #2

---
+++ tests/CardManagement.Tests/Common.fs
@@ -0,0 +1,28 @@
+﻿module CardManagement.Tests.Common
+
+open FsCheck
+
+// http://www.fssnip.net/7Vo/title/Chunk-String-by-Size-into-List-of-String-Chunks
+/// Chunk String by Size into List of String Chunks
+let chunkStr size str =
+    let rec loop (s:string) accum =
+        let branch = size < s.Length
+        match branch with
+        | true  -> loop (s.[size..]) (s.[0..size-1]::accum)
+        | false -> s::accum
+    (loop str []) |> List.rev
+
+let ``16DigitsStringGen`` =
+    Gen.choose (0,9)
+    |> Gen.listOfLength 16
+    |> Gen.map (List.map string >> String.concat "")
+
+let insertEvery n str = chunkStr n >> String.concat str
+
+/// generates pair ("0000000000000000", "0000 0000 0000 0000")
+type ``16DigitsStringAndWithSpacesEvery4Digits`` =
+    static member String() =
+        let insertSpacesEvery4Digits = " " |> insertEvery 4
+        ``16DigitsStringGen``
+        |> Gen.map (fun x -> x,x |> insertSpacesEvery4Digits)
+        |> Arb.fromGen
